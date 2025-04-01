c4f653e74a65217a563a5d229ef4233175867356
Yeon <127393133+Ahnsaeyeon@users.noreply.github.com>
2025-04-01 23:56:08 +0900

    Delete Lecture directory

--- Lecture/js1/5.string.js
+++
@@ -1,9 +0,0 @@
-/**
- * 문자열 타입
- */
-
-let string = "hi";
-let string2 = "hello";
-let string3 = `${string}, ${string2}`;
-console.log("string", string3);
-
--- Lecture/node1/index.js
+++
@@ -1,3 +0,0 @@
-import chalk from "./chalk/source";
-
-console.log(chalk.blue("Hello world!"));
